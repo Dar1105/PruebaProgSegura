84a87f8de68555051ecd7f638c4508e000aa50f7
darsy.morales <darsy.morales@correoaiep.cl>
2021-09-29 11:07:24 -0300

    Cambio:11_07am

--- WebApplication1/Repositories/IndexRepository.cs
+++ WebApplication1/Repositories/IndexRepository.cs
@@ -8,7 +8,7 @@ namespace ProgSeguraLogin.Repositories
 {
     public class IndexRepository
     {
-        private readonly string ConnectionString = "server=localhost; database=SistemaDeBusquedaBD;Integrated Security=true;";
+        private readonly string ConnectionString = "server=localhost; database=SistemaDeBusquedaDB;Integrated Security=true;";
 
         public bool ValidarUsuario(string usuario, string password)
         {
@@ -17,7 +17,7 @@ namespace ProgSeguraLogin.Repositories
             using SqlCommand cmd = new SqlCommand("select count(*) from usuarios where usuario = '" + usuario + "' and[password] = '" + password + "'", sql);
             int valor;
 
-            sql.Open(); //con esto se abre
+            sql.Open();
             valor = (int)cmd.ExecuteScalar();
             if (valor > 0)
             {
@@ -29,3 +29,23 @@ namespace ProgSeguraLogin.Repositories
         }
     }
     }
+/*
+
+public bool ExisteUsuario(string nombreUsuario, string password)
+{
+    bool respuesta = false;
+    string connectionString = "server=localhost;database=sistemaBusqueda2;Integrated Security = true;";
+    using SqlConnection sql = new SqlConnection(connectionString);
+    using SqlCommand cmd = new SqlCommand("sp_check_usuario", sql);
+    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+    cmd.Parameters.Add(new SqlParameter("@nombreUsuario", nombreUsuario));
+    cmd.Parameters.Add(new SqlParameter("@password", password));
+    sql.Open();
+    int resultadoQuery = (int)cmd.ExecuteScalar();
+    if (resultadoQuery > 0)
+    {
+        respuesta = true;
+    }
+    return respuesta;
+
+*/
